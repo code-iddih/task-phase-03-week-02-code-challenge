51d36ca340cd22f60e423d485552c58d32f611db
code-iddih <idinyamwaya@gmail.com>
2024-09-09 16:14:29 +0300

    Updating the Tests

--- coffee_shop/tests/test_coffee.py
+++ coffee_shop/tests/test_coffee.py
@@ -12,14 +12,14 @@ def test_coffee_order_count():
     # Creating a coffee instance with a valid name
     coffee = Coffee("Robusta")
     # Checking the initial number of orders
-    assert coffee.num_orders() == 0  # Should be 0 since no orders have been made
+    assert coffee.count_orders() == 0  # It should be 0
 
     # Creating a customer instance with a valid name
     customer = Customer("Eric")
     # Creating an order for the customer
     customer.create_order(coffee, 3.5)  # Customer orders an Espresso for $3.5
     # Checking the number of orders again
-    assert coffee.num_orders() == 1  # Should now be 1
+    assert coffee.count_orders() == 1  # It should now be 1
 
 def test_coffee_average_order_price():
     # Creating a coffee instance
@@ -31,9 +31,9 @@ def test_coffee_average_order_price():
     customer = Customer("Eric")
     # Creating multiple orders for the same coffee
     customer.create_order(coffee, 4.5) 
-    # First order lets say for $4.5
+    # First order lets say for 4.5
     customer.create_order(coffee, 5.5)
-    # then Second order for $5.5  
+    # then Second order for 5.5  
     # Calculating and checking the average price of the orders
     assert coffee.average_price() == 5.0  
     # Average of 4.5 and 5.5 should be 5.0
--- coffee_shop/tests/test_customer.py
+++ coffee_shop/tests/test_customer.py
@@ -10,18 +10,18 @@ def test_customer_name_validation():
 
     # Testing to ensure that customer name cannot exceed 15 characters
     with pytest.raises(ValueError):
-        Customer("ThisNameIsTooLong")  
+        Customer("Eric Nyamwaya Nyakangwa")  
 
 def test_customer_order_creation():
     # Creating a valid customer
-    customer = Customer("Roberta") 
+    customer = Customer("Eric") 
     # Using a specific coffee type for the order
     coffee = Coffee("Robusta")  
     # Creating an order with valid price
     order = customer.create_order(coffee, 5.5)  
 
     # Checking if the created order is in the customer's orders
-    assert order in customer.orders()
+    assert order in customer.orders
     # It should contain the created order
     
     # Checking if the cofffee ordered is included in the customer's coffees

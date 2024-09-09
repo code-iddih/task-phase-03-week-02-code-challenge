f8681183d1cc04d4bdc494a92cb214ba79223880
code-iddih <idinyamwaya@gmail.com>
2024-09-09 14:09:45 +0300

    Setting Up the File

---
+++ coffee_shop/order.py
@@ -0,0 +1,33 @@
+from customer import Customer
+from coffee import Coffee
+
+class Order:
+    def __init__(self, customer, coffee, price):
+        # Validating if the customer is a valid Customer instance
+        if not isinstance(customer, Customer):
+            raise ValueError("Invalid Customer.")
+        # Validating if the coffee is a valid Coffee instance
+        if not isinstance(coffee, Coffee):
+            raise ValueError("Invalid Coffee.")
+        # Validating the price is within the acceptable range
+        if not (1.0 <= price <= 10.0):
+            raise ValueError("Price must be between 1.0 and 10.0.")
+
+        # Assigning the attributes
+        self.customer = customer
+        self.coffee = coffee
+        self.price = price
+
+        # Adding this order to both coffee's and customer's order lists
+        coffee.orders().append(self)
+        customer.orders().append(self)
+
+    def __repr__(self):
+        # Returning Customer's Name , Coffee's nName and Price
+        return (
+            f"Order("
+            f"customer={self.customer.name}, "
+            f"coffee={self.coffee.name}, "
+            f"price={self.price})"
+        )
+
